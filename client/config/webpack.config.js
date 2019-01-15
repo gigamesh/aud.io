f9de922276138ce54de7afeda9f1857fc39c79ba
Matt Masurka <m.masurka@gmail.com>
2019-01-14 22:36:23 -0800

    Didn't work - reverting

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -467,13 +467,13 @@ module.exports = function(webpackEnv) {
       ]
     },
     plugins: [
-      isEnvProduction &&
-        new CompressionPlugin({
-          test: /\.(js|css)$/,
-          filename: "[path][query]",
-          algorithm: "gzip",
-          deleteOriginalAssets: true
-        }),
+      // isEnvProduction &&
+      //   new CompressionPlugin({
+      //     test: /\.(js|css)$/,
+      //     filename: "[path][query]",
+      //     algorithm: "gzip",
+      //     deleteOriginalAssets: true
+      //   }),
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
