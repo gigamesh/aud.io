b9ae8397f09fc21846582715a790b55795c4b225
Matt Masurka <m.masurka@gmail.com>
2019-05-25 21:59:01 -0700

    working

--- server/config/config.js
+++ server/config/config.js
@@ -4,11 +4,11 @@ const config = {
     DATABASE: process.env.MONGODB_URI
   },
   default: {
-    SECRET: 'MUSICISTHEANSWER',
-    DATABASE: 'mongodb://localhost:27017/aud_io'
+    SECRET: "MUSICISTHEANSWER",
+    DATABASE: "mongodb://mongo:27017/aud_io"
   }
-}
+};
 
-exports.get = function get(env){
-  return config[env] || config.default
-}
+exports.get = function get(env) {
+  return config[env] || config.default;
+};
